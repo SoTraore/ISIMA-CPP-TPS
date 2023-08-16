f1583b948205bcc2c94efc282562481bae843763
sotraore3 <sotraore3@ada.local.isima.fr>
2023-08-16 21:36:50 +0200

    Having the built in copy function not working well with what we're doing we propose our own to display our map elements

--- TP8/TableAssociative/Main.cpp
+++ TP8/TableAssociative/Main.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
 #include <queue>
+#include <map>
 #include <vector>
 #include <iterator>
+#include <algorithm>
 
+using namespace std;
+
+const string& first(const pair<string,string>& p) { 
+  return p.first; 
+}
+
+const string& second(const pair<string,string>& p) { 
+  return p.second; 
+}
+
+string toString(std::map<string, string> m) {
+  string result  = "";
+  for (auto& elt : m) {
+     result += "First : " + elt.first + ", Second : " + elt.second + "\n";
+  }
+  return result;
+}
+
+string afficher(pair<string, string> couple) {
+  string result = "";
+  result += couple.first + " " + couple.second + "\n";
+  return result;
+}
+
+template <class begin, class end>
+ostream& myCopy(begin b, end e, ostream& output) {
+  for (auto i=b; i!=e; i++) {
+    output << afficher(*i) ;
+  }
+  return output;
+}
 
 int main() {
+  using namespace std;
 
   typedef std::map<string, string> mss;
   // OU en C++ 2011
@@ -17,9 +51,16 @@ int main() {
   m["secours"]  = "42";
   mss::iterator it = m.find("loic");
   if (it==m.end()) cout << "et moi ?";
-  const string& first(const pair<string,string>& p) { return p.first; }
+  transform(m.begin(), m.end(), 
+		  ostream_iterator<string>(cout, "\n"), first);
+  transform(m.begin(), m.end(), 
+		  ostream_iterator<string>(cout, "\n"), second);
+  cout << toString(m);
+  myCopy(m.begin(), m.end(), cout << "\n");
+}
 
-  int main(int, char**) {
+/*
+int main(int, char**) {
   map<string, string> liste;
 
   map<string, string>::const_iterator it 
@@ -35,3 +76,4 @@ int main() {
   
   return 0;
 }
+*/
