160578cadebb101a65d09c2a0facb62ab1ffb375
sotraore3 <sotraore3@ada.local.isima.fr>
2023-08-19 09:12:33 +0200

    Making the parent class delete method virtual

---
+++ TP10/heritage.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Mere {
+ public:
+  Mere() {
+    // cout << "Mere::Mere()" << endl;
+    tab = new int[100];
+  } 
+  virtual ~Mere() {
+    // cout << "Mere::~Mere()" << endl;
+    delete [] tab;
+  }
+ protected:
+  int * tab;
+};
+
+class Fille : public Mere {
+ public:
+   Fille() {
+      // cout << "Fille:Fille()" << endl;
+      tab = new double[100];
+      //Mere::tab[50] = 4;
+   }
+   ~Fille() {
+      // cout << "Fille::~Fille()" << endl;
+      delete [] tab;
+   }
+ protected:
+   double * tab;
+};
+
+int main(int, char**) {
+  Mere * m = new Fille();
+  cout << "penser a valgrind" << endl;
+  delete m; 
+
+  return 0;
+}
