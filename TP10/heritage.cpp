af67bc6bfe3ffc3fbf92727bdd4a7aacdecf02a8
sotraore3 <sotraore3@ada.local.isima.fr>
2023-08-20 23:42:19 +0200

    Implement heritage legacy concept

--- TP10/heritage.cpp
+++ TP10/heritage.cpp
@@ -5,26 +5,41 @@ using namespace std;
 class Mere {
  public:
   Mere() {
-    // cout << "Mere::Mere()" << endl;
+    cout << "Mere::Mere()" << endl;
     tab = new int[100];
   } 
   virtual ~Mere() {
-    // cout << "Mere::~Mere()" << endl;
+    cout << "Mere::~Mere()" << endl;
     delete [] tab;
   }
  protected:
   int * tab;
 };
 
-class Fille : public Mere {
+class Pere {
+  public:
+    Pere() {
+      cout << "Pere::Pere()" << endl;
+      tab = new int[100];
+    }
+
+    virtual ~Pere() {
+      if (tab) delete [] tab;
+      cout << "Pere::~Pere()" << endl;
+    }
+  protected:
+    int * tab;
+};
+
+class Fille : public Mere, public Pere {
  public:
    Fille() {
-      // cout << "Fille:Fille()" << endl;
+      cout << "Fille:Fille()" << endl;
       tab = new double[100];
       //Mere::tab[50] = 4;
    }
    ~Fille() {
-      // cout << "Fille::~Fille()" << endl;
+      cout << "Fille::~Fille()" << endl;
       delete [] tab;
    }
  protected:
